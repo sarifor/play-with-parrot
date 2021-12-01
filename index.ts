0204cedca176ac46f160f098ddd32f450a6c717b
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-12-01 23:16:43 +0900

    Override methods

--- index.ts
+++ index.ts
@@ -31,25 +31,67 @@ class Parrot implements Animal {
     }
 
     checkCurrentStatus(): void {
-        console.log(this.name, this.stomach, this.words, this.songs, this.satisfied, this.angry);
+        console.log(`Name: ${this.name}, \nStomach: ${this.stomach}, \nWords: ${this.words}, \nSongs: ${this.songs}, \nSatisfied: ${this.satisfied}, \nAngry: ${this.angry}\n`);
+
+        if (this.satisfied > 4 && this.satisfied > this.angry) {
+            console.log(`${this.name} is satisfied as much as ${this.satisfied}. Happy End!\n`);
+        } else if (this.angry > 4 && this.satisfied < this.angry) {
+            console.log(`${this.name} is angry as much as ${this.angry}. Bed End!\n`);
+        } else {
+            console.log(`${this.name} is satisfied as much as ${this.satisfied}, and angry as much as ${this.angry}.\n`);
+        };
     };
 
     feed(): void {
         this.stomach = this.stomach + 1;
+        this.satisfied = this.satisfied + 2;
     };
 
     teachWords(words: string): void {
-        this.words.push(words);
-        console.log(`The latest word ${this.name} learned is ` +  this.words[this.words.length - 1]);
+        if (words.length >= 10) {
+            console.log(`The length of '${words}' is ${words.length}. Please teach words less than 10 letters`);
+        } else {
+            this.words.push(words);
+            this.satisfied = this.satisfied + 1;
+            console.log(`${this.name} is saying '${this.words[this.words.length - 1]}'`);    
+        };
     };
 
     pet(): void {
-        this.satisfied = this.satisfied + 1;
+        this.angry = this.angry + 1;
+        console.log(`Petted ${this.name}`);
     };
 
     teachSongs(songs: string): void {
-        this.songs.push(songs);
-        console.log(`${this.name} is singing ` + this.songs);
+        let num1: number;
+        let num2: number;
+        let smallNum: number;
+        let bigNum: number;
+
+        if (songs.length > 10) {
+            // 1 ~ songs 길이 사이의 랜덤 숫자 2개 생성하여, songs에서 일부 글자만 골라내고(substr), songs 배열에 저장
+            num1 = Math.random() * songs.length;
+            num2 = Math.random() * songs.length;
+
+            if(num1 > num2) {
+                smallNum = num2;
+                bigNum = num1;
+            } else {
+                smallNum = num1;
+                bigNum = num2;
+            };
+
+            if (smallNum < bigNum) {
+                songs = songs.substr(smallNum, bigNum);
+                this.songs.push(songs);
+                this.satisfied = this.satisfied + 1;
+                console.log(`${this.name} is singing '${this.songs}', which is edited by itself!`);
+            } else {
+                console.log("It refused to learn songs! Try again.");
+            };
+        } else {
+            console.log(`The length of '${songs}' is ${songs.length}. Please teach song more than 10 letters.`);
+        };
     };
 
     static whoIsHappier(parrot1: Parrot, parrot2: Parrot): string {
@@ -62,17 +104,41 @@ class Parrot implements Animal {
         } else if (parrot1.satisfied === parrot2.satisfied) {
             result = `${parrot1.name} and ${parrot2.name} are at the same level of happiness!`;
         } else {
-            result = "It is difficult to find who is happier";
+            result = "It is difficult to find who is happier.";
         };
 
         return result;
-    }
+    };
 };
 
 const louis: Parrot = new Parrot("Louis");
 const romi: Parrot = new Parrot("Romi");
 
-louis.teachWords("Hello World!");
+console.log("[Teaching words]");
+louis.teachWords("Hello!");
+louis.teachWords("My tumblr!");
+louis.teachWords("WHY?");
+louis.teachWords("Where is papa?");
+louis.teachWords("Good luck!");
 
+console.log("");
+console.log("[Petting]");
+louis.pet();
+louis.pet();
+louis.pet();
+louis.pet();
+louis.pet();
+
+console.log("");
+console.log("[Teaching songs]");
+louis.teachSongs("When I find myself in times of trouble, Mother Mary comes to me");
+
+console.log("");
+console.log("[Checking current status]");
+louis.checkCurrentStatus();
+romi.checkCurrentStatus();
+
+console.log("");
+console.log("[Who is happier?]");
 let result: string = Parrot.whoIsHappier(louis, romi);
 console.log(result);
